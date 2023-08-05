21c7ccc1b90c165164f947b092b133cd21f64a61
Martin Petrov <martinmpetrov@hotmail.com>
2023-08-05 17:24:28 +0300

    Change statistics

--- src/containers/home/Statistics.tsx
+++ src/containers/home/Statistics.tsx
@@ -15,18 +15,18 @@ const Statistics = () => {
         </div>
         <div className="flex flex-col grow items-center gap-2 py-10 px-12 dark:dark:!bg-base-700/10 bg-base-500/10 border-b border-r-0 md:border-b-0 md:border-r">
           <span className="text-6xl bg-gradient-to-tr from-primary-500 to-secondary-500 bg-clip-text text-transparent font-black">
-            484+
-          </span>
-          <span className="text-lg dark:text-base-300 text-base-900">
-            Themes out of the box
-          </span>
-        </div>
-        <div className="flex flex-col grow items-center gap-2 py-10 px-12 dark:dark:!bg-base-700/10 bg-base-500/10">
-          <span className="text-6xl bg-gradient-to-tr from-primary-500 to-secondary-500 bg-clip-text text-transparent font-black">
             20+
           </span>
           <span className="text-lg dark:text-base-300 text-base-900">
             React hooks
+          </span>
+        </div>
+        <div className="flex flex-col grow items-center gap-2 py-10 px-12 dark:dark:!bg-base-700/10 bg-base-500/10">
+          <span className="text-6xl bg-gradient-to-tr from-primary-500 to-secondary-500 bg-clip-text text-transparent font-black">
+            210+
+          </span>
+          <span className="text-lg dark:text-base-300 text-base-900">
+            Vibrant colors
           </span>
         </div>
       </div>
